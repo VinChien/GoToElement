ec3df2a4ea88218fbe234b5e983c3204919de723
jianfan0114@gmail.com <jianfan0114@gmail.com>
2020-07-12 14:25:22 +0800

    完成

--- goToElement.js
+++ goToElement.js
@@ -1,17 +1,29 @@
 window.onload = () => {
 
-    // console.log("頁面載入完成~")
+    // 取得單一元素
     // var test = document.getElementById("test");
 
-    test.onclick=()=>{
-        var target = test.getAttribute("data-gte-target");
-        var offset = test.getAttribute("data-gte-offset");
+    // 取得所有元素，同時也是個陣列
+    var elements = document.getElementsByTagName("*");
 
-        var element = document.getElementById(target);
-
-        window.scrollTo({
-            top: element.offsetTop-offset,
-            behavior: "smooth" 
-        })
+    // 搜尋有data-gte的元素
+    for (let i = 0; i < elements.length; i++) {
+        const e = elements[i];
+        
+        e.onclick=()=>{
+            var target = e.getAttribute("data-gte-target");
+            var offset = e.getAttribute("data-gte-offset");
+    
+            // 用 if 過濾沒有data-gte的元素
+            if (target){
+                var element = document.getElementById(target);
+                
+                // 畫面移動
+                window.scrollTo({
+                    top: element.offsetTop-offset,
+                    behavior: "smooth" 
+                });
+            }
+        }
     }
 }
